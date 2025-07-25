3d4b7521dc76f8fbb9065491fe6b38fbd5112bab
Anastasiia Kharchenko <anastasiia@MacBook-Air-Anastasiia.local>
2025-07-25 12:16:25 +0300

    Додано привітання /start, багатомовність, перевірка підписок

--- i18n.js
+++ i18n.js
@@ -3,25 +3,26 @@
 const translations = {
   choose_plan: {
     uk: 'Оберіть тип підписки:',
-    en: 'Choose a subscription plan:'
+    en: 'Choose a subscription plan:',
   },
   payment_description: {
     uk: 'Оплата за доступ до додаткових нагадувань',
-    en: 'Payment for extra birthday reminders'
+    en: 'Payment for extra birthday reminders',
   },
   subscription_activated: {
     uk: (date) => `✅ Підписку активовано до ${date}`,
-    en: (date) => `✅ Subscription active until ${date}`
+    en: (date) => `✅ Subscription active until ${date}`,
   },
   help: {
     uk: `🆘 Допомога\n\nЦей бот допоможе вам не забути привітати друзів з Днем народження! Додайте нагадування, і бот нагадає вам у потрібний день.\n\nБезкоштовно доступно 3 нагадування, далі — підписка.`,
-    en: `🆘 Help\n\nThis bot helps you remember birthdays! Add reminders, and you'll be notified on the day.\n\nYou can save 3 reminders for free, then a subscription is required.`
-  }
+    en: `🆘 Help\n\nThis bot helps you remember birthdays! Add reminders, and you'll be notified on the day.\n\nYou can save 3 reminders for free, then a subscription is required.`,
+  },
   start: {
-  uk: 'Привіт! Я SayHBDbot 🎉\nДодай нагадування і тепер ти не забудеш когось привітати',
-  en: 'Hello! I\'m SayHBDbot 🎉\nAdd a reminder so you won’t forget to say happy birthday!'
-},
+    uk: 'Привіт! Я SayHBDbot 🎉\nДодай нагадування і тепер ти не забудеш когось привітати',
+    en: 'Hello! I\'m SayHBDbot 🎉\nAdd a reminder so you won’t forget to say happy birthday!',
+  },
 };
+
 
 function getLang(msg) {
   const userId = msg.from?.id || msg.chat?.id;
